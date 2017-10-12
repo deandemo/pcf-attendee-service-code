433fcd0fa65a7c39c961d63448641b821ca62c18
Dean Baker <dean.baker@pexa.com.au>
2017-10-12 10:23:33 -0600

    Revert to one job

--- ci/tasks/smoke-test.sh
+++ ci/tasks/smoke-test.sh
@@ -3,10 +3,10 @@
 set -e +x
 
 pushd attendee-service
-  echo "Executing smoke tests [$1]"
+  echo "Executing smoke tests [$TARGET]"
 popd
 
 #find attendee-service/target/surefire-reports -type f -name *.txt -exec cp "{}" test-output/unit-test.txt \;
 
-echo "Done smoke testing [$1]"
+echo "Done smoke testing [$TARGET]"
 exit 0
