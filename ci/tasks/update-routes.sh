8c660571ffa4cf82c97888ac32b60a2c742c5a28
Dean Baker <dean.baker@pexa.com.au>
2017-10-12 14:04:10 -0600

    Adding smoke test

--- ci/tasks/update-routes.sh
+++ ci/tasks/update-routes.sh
@@ -5,31 +5,32 @@ set -xe
 pwd
 env
 
-cf api $CF_API --skip-ssl-validation
+cf api $PWS_API --skip-ssl-validation
 
-cf login -u $CF_USER -p $CF_PWD
+cf login -u $PWS_USER -p $PWS_PWD -o "$PWS_ORG" -s "$PWS_SPACE"
 
 cf apps
 
 cf routes
 
-export CF_DOMAIN_NAME=$CF_APP_DOMAIN
-export MAIN_ROUTE_HOSTNAME=main-$CF_APP_SUFFIX
+export PWS_DOMAIN_NAME=$PWS_APP_DOMAIN
+export MAIN_ROUTE_HOSTNAME=main-$PWS_APP_SUFFIX
 
 export NEXT_APP_COLOR=$(cat ./current-app-info/next-app.txt)
-export NEXT_APP_HOSTNAME=$NEXT_APP_COLOR-$CF_APP_SUFFIX
+export NEXT_APP_HOSTNAME=$NEXT_APP_COLOR-$PWS_APP_SUFFIX
 
 export CURRENT_APP_COLOR=$(cat ./current-app-info/current-app.txt)
-export CURRENT_APP_HOSTNAME=$CURRENT_APP_COLOR-$CF_APP_SUFFIX
+export CURRENT_APP_HOSTNAME=$CURRENT_APP_COLOR-$PWS_APP_SUFFIX
 
-echo "Mapping main app route to point to BLUE instance"
-cf map-route attendee-service-blue apps.cumuluslabs.io -n attendee-service-propanedioic-proportion
+echo "Mapping main app route to point to $NEXT_APP_HOSTNAME instance"
+cf map-route $NEXT_APP_HOSTNAME $PWS_DOMAIN_NAME --hostname $MAIN_ROUTE_HOSTNAME
+
 cf routes
 
 echo "Removing previous main app route that pointed to $CURRENT_APP_HOSTNAME instance"
 
 set +e
-cf unmap-route attendee-service apps.cumuluslabs.io -n attendee-service-propanedioic-proportion
+cf unmap-route $CURRENT_APP_HOSTNAME $PWS_DOMAIN_NAME --hostname $MAIN_ROUTE_HOSTNAME
 set -e
 
 echo "Routes updated"
